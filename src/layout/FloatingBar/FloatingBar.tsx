8a45e9db825b685075e8783ddec4149028e1200d
sunhyuk.chang <wkdtjsgur100@naver.com>
2024-05-15 18:24:22 +0900

    firebase 연동

--- src/layout/FloatingBar/FloatingBar.tsx
+++ src/layout/FloatingBar/FloatingBar.tsx
@@ -1,27 +1,28 @@
 // import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import data from 'data.json';
-// import { increment, onValue, ref, update } from 'firebase/database';
-// import { realtimeDb } from 'firebase.ts';
+import { increment, onValue, ref, update } from 'firebase/database';
+import { realtimeDb } from 'firebase.ts';
 import JSConfetti from 'js-confetti';
 import Heart from '@/assets/icons/heart_plus.svg?react';
 import Share from '@/assets/icons/share.svg?react';
 import Upward from '@/assets/icons/upward.svg?react';
 import Button from '@/components/Button.tsx';
+import {useEffect, useState} from "react";
 
 const FloatingBar = ({ isVisible }: { isVisible: boolean }) => {
   const { emojis } = data;
 
   // TODO: count 기능 사용 원할시 firebase realtime db 연결!
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-  // useEffect(() => {
+  useEffect(() => {
   // TODO: realtime db 에 likes 객체 추가.
-  //   const dbRef = ref(realtimeDb, 'likes');
-  //   onValue(dbRef, (snapshot) => {
-  //     setCount(Number(snapshot.val()));
-  //   });
-  // }, []);
+    const dbRef = ref(realtimeDb, 'likes');
+    onValue(dbRef, (snapshot) => {
+      setCount(Number(snapshot.val()));
+    });
+  }, []);
 
   const handleCopy = () => {
     navigator.clipboard.writeText(window.location.href).then(
@@ -38,10 +39,10 @@ const FloatingBar = ({ isVisible }: { isVisible: boolean }) => {
     void jsConfetti.addConfetti({ emojis });
 
     // 버튼 클릭시 likes 수 증가
-    // const dbRef = ref(realtimeDb);
-    // void update(dbRef, {
-    //   likes: increment(1),
-    // });
+    const dbRef = ref(realtimeDb);
+    void update(dbRef, {
+      likes: increment(1),
+    });
   };
 
   const jsConfetti = new JSConfetti();
