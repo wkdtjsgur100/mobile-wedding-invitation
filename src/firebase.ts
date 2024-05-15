8b6244a96bcd8835e46148a26d58ca2b96a84822
sunhyuk.chang <wkdtjsgur100@naver.com>
2024-05-15 18:12:10 +0900

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
 
 // TODO: Add SDKs for Firebase products that you want to use
